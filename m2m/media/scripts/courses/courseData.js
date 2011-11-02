ab6b90d77f53151b7b724cce97b0995f68f3f0a9
Haak Saxberg <haak.erling@gmail.com>
2011-11-01 19:19:22 -0700

    media to go along with whatever the previous commit was.

---
+++ m2m/media/scripts/courses/courseData.js
@@ -0,0 +1,77 @@
+var courses = new Array();
+
+courses["CSCI005HM-01"] = {id: "CSCI005HM-01", title: "CS 5, Dodds", start: "Tue, 6 Sep 2011 8:35:00 PST", end: "Tue, 6 Sep 2011 9:50:00 PST", registered: false, credits: 3,prerequisites: true, pending:false, };
+
+courses["CSCI005HM-01a"] = {id: "CSCI005HM-01", title: "CS 5, Dodds", start: "Thu, 8 Sep 2011 8:35:00 PST", end: "Thu, 8 Sep 2011 9:50:00 PST", registered: false, credits: 3, prerequisites: true, pending:false, };
+
+courses["CSCI005LHM-04"] = {id: "CSCI005LHM-04", title: "CS 5 Lab, Hadas", start: "Tue, 6 Sep 2011 17:00:00 PST", end: "Tue, 6 Sep 2011 19:00:00 PST", registered: false, credits: 0,prerequisites: true, pending:false, };
+
+courses["CSCI060HM-01"] = {id: "CSCI060HM-01", title: "CS 60, Dodds", start: "Mon, 5 Sep 2011 12:15:00 PST", end: "Mon, 5 Sep 2011 13:30:00 PST", registered: false, credits: 3, prerequisites: false, pending:false, };
+
+courses["CSCI060HM-01a"] = {id: "CSCI060HM-01", title: "CS 60, Dodds", start: "Wed, 7 Sep 2011 12:15:00 PST", end: "Wed, 7 Sep 2011 13:30:00 PST", registered: false, credits: 3, pending:false, };
+
+courses["MUS 085ASC-01"] = {id: "MUS 085ASC-01", title: "MUS 85A, Simon", start: "Tue, 6 Sep 2011 15:15:00 PST", end: "Tue, 6 Sep 2011 16:30:00 PST", registered: false, credits: 1.5,prerequisites: true, pending:false, };
+
+courses["MUS 085CSC-01"] = {id: "MUS 085CSC-01", title: "MUS 85C, Simon", start: "Wed, 7 Sep 2011 09:15:00 PST", end: "Wed, 7 Sep 2011 10:30:00 PST", registered: false, credits: 1.5,prerequisites: true, pending:false, };
+
+courses["MUS 171ASC-01"] = {id: "MUS 171ASC-01", title: "MUS 171A, Huang", start: "Fri, 9 Sep 2011 12:00:00 PST", end: "Fri, 9 Sep 2011 13:00:00 PST", registered: false, credits: 1.5,prerequisites: false, pending:false, };
+
+courses["MUS 171CSC-02"] = {id: "MUS 171CSC-02", title: "MUS 171A, Huang", start: "Fri, 9 Sep 2011 12:00:00 PST", end: "Fri, 9 Sep 2011 13:00:00 PST", registered: false, credits: 1.5,prerequisites: false, pending:false, };
+
+courses["ECON051PO-01"] = {id: "ECON051PO-01", title: "ECON 51A, Chincar", start: "Mon, 5 Sep 2011 12:15:00 PST", end: "Mon, 5 Sep 2011 13:30:00 PST", registered: false, credits: 3,prerequisites: true, pending:false, };
+
+courses["ECON051PO-01a"] = {id: "ECON051PO-01", title: "ECON 51, Chincar", start: "Wed, 7 Sep 2011 12:15:00 PST", end: "Wed, 7 Sep 2011 13:30:00 PST", registered: false, credits: 3,prerequisites: true, pending:false, };
+
+courses["ECON101CM-01"] = {id: "ECON101CM-01", title: "ECON 101, Hirakawa", start: "Mon, 5 Sep 2011 11:00:00 PST", end: "Mon, 5 Sep 2011 12:10:00 PST", registered: false, credits: 3,prerequisites: true, pending:false, };
+
+courses["ECON101CM-01a"] = {id: "ECON101CM-01", title: "ECON 101, Hirakawa", start: "Wed, 7 Sep 2011 11:00:00 PST", end: "Wed, 7 Sep 2011 12:10:00 PST", registered: false, credits: 3,prerequisites: true, pending:false, };
+
+courses["ECON102CM-01"] = {id: "ECON102CM-01", title: "ECON 102, Burdekin", start: "Mon, 5 Sep 2011 11:00:00 PST", end: "Mon, 5 Sep 2011 12:10:00 PST", registered: false, credits: 3,prerequisites: false, pending:false, };
+
+courses["ECON102CM-01a"] = {id: "ECON102CM-01", title: "ECON 102, Burdekin", start: "Wed, 7 Sep 2011 11:00:00 PST", end: "Wed, 7 Sep 2011 12:10:00 PST", registered: false, credits: 3,prerequisites: false, pending:false, };
+
+courses["ECON104HM-01"] = {id: "ECON104HM-01", title: "ECON 104, Evans", start: "Tue, 6 Sep 2011 12:15:00 PST", end: "Tue, 6 Sep 2011 13:30:00 PST", registered: false, credits: 3,prerequisites: true, pending:false, };
+
+courses["ECON104HM-01a"] = {id: "ECON104HM-01", title: "ECON 104, Evans", start: "Thu, 8 Sep 2011 12:15:00 PST", end: "Thu, 8 Sep 2011 13:30:00 PST", registered: false, credits: 3, pending:false, };
+
+courses["ECON1111SC-01"] = {id: "ECON1111SC-01", title: "ECON 111, Flynn", start: "Mon, 5 Sep 2011 13:45:00 PST", end: "Mon, 5 Sep 2011 15:00:00 PST", registered: false, credits: 3,prerequisites: false, pending:false, };
+
+courses["ECON111SC-01a"] = {id: "ECON111SC-01", title: "ECON 111, Flynn", start: "Wed, 7 Sep 2011 13:45:00 PST", end: "Wed, 7 Sep 2011 15:00:00 PST", registered: false, credits: 3, pending:false, };
+
+courses["ECON121PO-01"] = {id: "ECON121PO-01", title: "ECON 121, Brown", start: "Mon, 5 Sep 2011 12:15:00 PST", end: "Mon, 5 Sep 2011 13:30:00 PST", registered: false, credits: 3,prerequisites: false, pending:false, };
+
+courses["ECON121PO-01a"] = {id: "ECON121PO-01", title: "ECON 121, Brown", start: "Wed, 7 Sep 2011 12:15:00 PST", end: "Wed, 7 Sep 2011 13:30:00 PST", registered: false, credits: 3,prerequisites: false, pending:false, };
+
+courses["ECON127PO-01"] = {id: "ECON127PO-01", title: "ECON 121, Cutter", start: "Mon, 5 Sep 2011 13:45:00 PST", end: "Mon, 5 Sep 2011 15:00:00 PST", registered: false, credits: 3,prerequisites: false, pending:false, };
+
+courses["ECON127PO-01a"] = {id: "ECON127PO-01", title: "ECON 121, Cutter", start: "Wed, 7 Sep 2011 13:45:00 PST", end: "Wed, 7 Sep 2011 15:00:00 PST", registered: false, credits: 3, pending:false, };
+
+var courseDescriptions = new Array();
+
+courseDescriptions["CSCI005HM-01"] = "<h2>CS 5. Introduction to Computer Science</h2> <dl> <dt>Requirements</dt><dd>Computer Science core course required for all HMC students.</dd>   <dt>Credit Hours</dt><dd>3.0</dd>     <dt>Seats Left</dt><dd>18/20.</dd> <dt>Decription</dt><dd>Introduction to elements of computer science. Students learn general computational problem-solving techniques and gain experience with the design, implementation, testing and documentation of programs in a high-level language. In addition, students learn to design digital devices, understand how computers work, and learn to program a computer in its own machine language. Finally, students are exposed to ideas in computability theory. The course includes discussions of societal and ethical issues related to computer science. </dd></dl> ";
+
+courseDescriptions["CSCI005LHM-04"] = "<h2>CS 5 Lab. Introduction to Computer Science Lab</h2> <dl>         <dt>Requirements</dt><dd>Concurrent enrollment in CS 5.</dd>     <dt>Credit Hours</dt><dd>0.0</dd>     <dt>Seats Left</dt><dd>18/20.</dd> </dl> ";
+
+courseDescriptions["CSCI060HM-01"] = "<h2>CS 60. Principles of Computer Science</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: CS 5 (or equivalent) and one semester of calculus or permission of instructor.</dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>12/20.</dd> <dt>Decription</dt><dd>Introduction to principles of computer science. Information structures, functional programming, object-oriented programming, grammars, logic, logic programming, correctness, algorithms, complexity analysis, finite-state machines, basic processor architecture, and theoretical limitations. Those who have completed Computer Science 42 cannot take Computer Science 60.</dd></dl> ";
+
+courseDescriptions["MUS 085ASC-01"] = "<h2>MUS 85a. Group Piano: 1st & 2nd Year</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: Audition or instructor permission. </dd> <dt>Credit Hours</dt><dd>1.5</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd>Beginning instruction in a digital piano lab setting with groups consisting of no more than nine students. Emphasis on performing solo and duet repertoire as well as learning to sight read, transpose, and provide harmonic accompaniment.</dd> </dl> ";
+
+courseDescriptions["MUS 085CSC-01"] = "<h2>MUS 85c. Group Piano: 3rd & 4th Year</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: Audition or instructor permission. </dd> <dt>Credit Hours</dt><dd>1.5</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd>Beginning and intermediate instruction in a digital piano lab setting with groups consisting of no more than nine students. Emphasis on performing solo and duet repertoire as well as learning to sight read, transpose, and provide harmonic accompaniment.</dd></dl> ";
+
+courseDescriptions["MUS 171ASC-01"] = "<h2>MUS 171a. Piano: 1st & 2nd Year</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: Audition or instructor permission. </dd> <dt>Credit Hours</dt><dd>1.5</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd> Individual instruction at the intermediate and advanced levels.</dd></dl> ";
+
+courseDescriptions["MUS 171CSC-02"] = "<h2>MUS 171c. Piano: 3rd & 4th Year</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: Audition or instructor permission. </dd> <dt>Credit Hours</dt><dd>1.5</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd> Individual instruction at the intermediate and advanced levels.</dd></dl> ";
+
+courseDescriptions["ECON051PO-01"] = "<h2>ECON 51. Principles: Macroeconomics</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: None. </dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>12/20.</dd> <dt>Description</dt><dd> A first course on modern market economies. Emphasizes the determination of national income, fluctuations and growth; the monetary system; the problems of inflation and unemployment; and international trade.</dd></dl> ";
+
+courseDescriptions["ECON101CM-01"] = "<h2>ECON 101. Intermediate Microeconomics</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: Economics 50 and some calculus. </dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>12/20.</dd> <dt>Description</dt><dd> An analysis of the determination of price and output under various market conditions, from competition to monopoly. Theories of economic choice are applied to consumers, producers, and resource owners. Techniques of partial equilibrium analysis are stressed.</dd></dl> ";
+
+courseDescriptions["ECON102CM-01"] = "<h2>ECON 102. Intermediate Microeconomics</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: Economics 50 and some calculus. Non-CMC need permission. </dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>12/20.</dd> <dt>Description</dt><dd> An analysis of the determination of price and output under various market conditions, from competition to monopoly. Theories of economic choice are applied to consumers, producers, and resource owners. Techniques of partial equilibrium analysis are stressed.</dd></dl> ";
+
+courseDescriptions["ECON104HM-01"] = "<h2>ECON 104. Financial Economics</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: Economics 53. </dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd> The principles of money and banking from the viewpoint of both business person and banker. Topics include the operation of commercial banks, related financial institutions, the development of the banking system, international finance, governmental fiscal and monetary policy, and the relations of money and credit to prices.</dd></dl> ";
+
+courseDescriptions["ECON111SC-01"] = "<h2>ECON 111. Behavioral Economics</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: ECON 51 & 101. </dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd> This class will introduce students to the field of behavioral economics, which combines ideas from psychology with experimental and empirical results to get a better handle on human behavior than has been supplied by traditional economics theory. Laboratory fee: $50.</dd></dl> ";
+
+courseDescriptions["ECON121PO-01"] = "<h2>ECON 121. Economics of Gender & the Family</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: ECON 52 or 102. </dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd> The Economics of Gender and the Family. Analysis of the factors contributing to the economic circumstances of women and men in modern market economies, especially the United States. Trends in labor-force participation, occupational choice and the economic determinants of earnings, household income and poverty.</dd></dl> ";
+
+courseDescriptions["ECON127PO-01"] = "<h2>ECON 127. Environmental Economics</h2> <dl> <dt>Requirements</dt><dd>Prerequisites: ECON 52 or 102. </dd> <dt>Credit Hours</dt><dd>3.0</dd> <dt>Seats Left</dt><dd>18/20.</dd> <dt>Description</dt><dd> Positive and normative issues involving the optimal regulation of pollution. Analysis of environmental laws and policies and the institutions that implement these policies. Examination of incentive-based pollution control policies such as cap and trade and pollution taxes. Consideration of economic and ecological approaches towards sustainability.</dd></dl> ";
