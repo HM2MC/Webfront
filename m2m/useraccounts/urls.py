25a993ea7ab8289a8ef9493de2be464a7ffc11b3
Haak Saxberg <haak.erling@gmail.com>
2012-03-02 20:56:40 -0800

    made logout redirect to login page (keeping away from staff site); dummy logout template in case further customization is needed.

--- m2m/useraccounts/urls.py
+++ m2m/useraccounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout_then_login, password_change, password_change_done
 
 urlpatterns = patterns('useraccounts.views',
                        (r'^$', 'view_home'),
                        (r'^profile/$', 'view_home'),
+                       url(r'^reset_password/$', password_change),
+                       url(r'^reset_password_done/$', password_change_done),
                        url(r'^login/$', login, name="login"),
-                       url(r'^logout/$', logout, name="logout"),
+                       url(r'^logout/$', logout_then_login, name="logout"),
                        )
